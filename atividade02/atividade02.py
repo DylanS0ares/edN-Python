ae353f69a14b856b208513c9207f4671a8f7934f
DylanS0ares <bdylansoares@gmail.com>
2025-08-21 09:58:35 -0300

    questao 02

--- atividade02/atividade02.py
+++ atividade02/atividade02.py
@@ -12,3 +12,18 @@ valor_em_euro = valor_em_reais / taxa_euro
 print(f"Valor em reais: R$",valor_em_reais)
 print(f"Valor em dólares: $",round(valor_em_dolar,2))
 print(f"Valor em euros: €",round(valor_em_euro,2))
+
+
+#Questão 02
+
+nome = "Camiseta"
+preco_original = 50.00
+porcentagem_desconto = 0.2
+# 2- Calculadora de Desconto
+
+valor_de_desconto = preco_original * porcentagem_desconto
+preco_final = preco_original - valor_de_desconto
+print(f"Produto: {nome}")
+print(f"Preço original: R$ {preco_original:.2f}")
+print(f"Valor do desconto: R$ {valor_de_desconto:.2f}")
+print(f"Preço final com desconto: R$ {preco_final:.2f}")
