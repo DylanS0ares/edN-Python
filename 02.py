e80b6d9cebe74412f73e17718e9a4dcb6bb57a5a
DylanS0ares <bdylansoares@gmail.com>
2025-08-27 10:37:28 -0300

    Atividade06_02

--- 02.py
+++ 02.py
@@ -1,15 +1,21 @@
 import requests
 def gerar_perfil_usuario():
     url = 'https://randomuser.me/api/'
-    resposta = requests.get(url)
-    dados = resposta.json()
+    try:
+        resposta = requests.get(url)
+        resposta.raise_for_status()
+        dados = resposta.json()
+        
+        usuario = dados['results'][0]
+        nome = f"{usuario['name']['first']} {usuario['name']['last']}"
+        email = usuario['email']
+        pais = usuario['location']['country']
+        return nome, email, pais
     
-    usuario = dados['results'][0]
-    nome = f"{usuario['name']['first']} {usuario['name']['last']}"
-    email = usuario['email']
-    pais = usuario['location']['country']
+    except (requests.RequestException, KeyError) as e:
+        print("Erro ao obter dados do usuário:", e)
+        return None, None, None
     
-    return nome, email, pais
 nome, email, pais = gerar_perfil_usuario()
 print("Nome:", nome)
 print("Email:", email)
